b88da1bcbc33e94af5505c846fcbf6a7ff5c13f8
Chun <chuntatchan123@gmail.com>
2018-06-20 13:35:43 -0700

    Initial commit

---
+++ Assets/Scripts/StoryLines.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections;
+
+public class StoryLines : MonoBehaviour {
+	public StoryStrings[] StoryLinesArray;
+}
+
+[System.Serializable]
+public class StoryStrings {
+	public string[] StoryText;
+}
---
+++ Assets/Scripts/VNMain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VNMain : MonoBehaviour {
+
+	public StoryLines storyLines;
+	public Text tbox;
+	public GameObject[] Characters;
+
+	private bool isPaused;
+	[SerializeField]
+	private int linesCounter;
+
+	private bool[] isDecisionActive;
+	private StoryStrings storyStrings;
+
+	// Use this for initialization
+	void Start () {
+		storyStrings = storyLines.StoryLinesArray [0];
+		isPaused = false;
+		linesCounter = 0;
+		tbox.text = storyStrings.StoryText [linesCounter];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isPaused) {
+			if (Input.GetButtonDown ("nextLine")) {
+				linesCounter++;
+				tbox.text = storyStrings.StoryText [linesCounter];
+			}
+		}
+	}
+}
